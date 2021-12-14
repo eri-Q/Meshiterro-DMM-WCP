2389792e194114b925b2ceab9ca5083ca8e6cb52
eri-Q <eri.hatanaka8@gmail.com>
2021-12-14 10:01:09 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '79a4b177021aa811ccc332810463d8d039beb76b3fb1472020cdc40e05f582803fc51c48357cdda9a97d86844f1b84ef3a1034ac7ff0493c97056f3112878a5e'
+
+
